3bcc0e02625b68e53d92d3f796e59aa9826495bf
MargaritaAlvarezGonz <margarita.alvarez3105@gmail.com>
2023-08-02 07:02:46 -0600

    added routing

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-inicio',
@@ -9,9 +10,29 @@ export class InicioComponent implements OnInit {
 
   edad = 25;
   peso = 60;
-  constructor() { }
+  altura = 170;
+  sexo = '';
+
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  cambiarAltura(event:any){
+   this.altura = event.target.value
+  }
+
+  masculino(){
+    this.sexo = 'Masculino';
+  }
+
+  femenino(){
+    this.sexo = 'Femenino';
+  }
+
+  calcularBMI(){
+    
+
   }
 
 }
