95eceb0131fba4eacc20135e56ae843ddf5a79d8
MargaritaAlvarezGonz <margarita.alvarez3105@gmail.com>
2023-08-02 12:41:04 -0600

    final version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { ResultadoComponent } from './components/resultado/resultado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent
+    InicioComponent,
+    ResultadoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -31,7 +31,9 @@ export class InicioComponent implements OnInit {
   }
 
   calcularBMI(){
-    
+    const BMI = this.peso /Math.pow(this.altura/100,2);
+    console.log(BMI.toFixed(1));
+    this.router.navigate(['/resultado', BMI.toFixed(1)]);
 
   }
 
